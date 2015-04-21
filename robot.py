3c0a30f09a0fbb81312f6f3d4c2b495fcf7cc9fe
Sean Rettig <seanrettig@gmail.com>
2015-04-21 15:11:06 -0700

    commit robot.py so far for testing

--- robot.py
+++ robot.py
@@ -13,36 +13,52 @@ PACKET_DEVICE = 0b00
 PACKET_RELOAD = 0b01
 PACKET_SWAT = 0b10
 
-speeds = {}
+class SerialManager(object):
 
-port = serial.Serial("/dev/ttyACM0", baudrate=9600, timeout=3.0)
+    def __init__(self):
+        self.speeds = {}
+        self.port = serial.Serial("/dev/ttyACM0", baudrate=9600, bytesize=serial.EIGHTBITS, parity=serial.PARITY_NONE, stopbits=serial.STOPBITS_ONE, timeout=3.0)
 
-def readPackets():
-    while port.inWaiting() > 0:
-        packet = port.read(2)
-        byte1, byte2 = map(int, packet)
-        deviceid = byte1 << (8 - OFFSET_TYPE) >> (8 - OFFSET_TYPE + OFFSET_ID) << OFFSET_ID
-        magnitude = byte2
-        print "device {} updated with magnitude {}".format(deviceid, magnitude)
+    def bytes_to_strings(self, packet):
+        return [format(int(p), '#010b') for p in packet[1:-1].split(', ')]
 
-def getSpeed(deviceid):
-    return speeds[deviceid]
+    def readPackets(self):
+        while self.port.inWaiting() > 0:
+            packet = self.port.read(2)
+            print "received: '{}'".format([ord(p) for p in packet])
+            byte1, byte2 = map(ord, packet)
+            deviceid = byte1 << (8 - OFFSET_TYPE) >> (8 - OFFSET_TYPE + OFFSET_ID) << OFFSET_ID
+            magnitude = byte2
+            dbyte, mbyte = 1, 2#self.bytes_to_strings(packet)
+            #print "device {} ({}) updated with magnitude {} ({})".format(deviceid, dbyte, magnitude, mbyte)
 
-def setSpeed(deviceid, speed=0, direction=0):
-    byte1 = (PACKET_DEVICE << OFFSET_TYPE) | (deviceid << OFFSET_ID) | (direction << OFFSET_DIRECTION)
-    byte2 = (speed << OFFSET_MAGNITUDE)
-    packet = bytes([byte1, byte2])
-    print packet
-    print [format(int(p), '#010b') for p in packet[1:-1].split(', ')]
-    port.write(bytes([byte1, byte2]))
+    def getSpeed(self, deviceid):
+        return self.speeds[deviceid]
 
-while True:
-    readPackets()
-    print getSpeed(1)
-    setSpeed(1, 255, 0)
-    time.sleep(1)
+    def setSpeed(self, deviceid, speed=0, direction=0):
+        byte1 = (PACKET_DEVICE << OFFSET_TYPE) | (deviceid << OFFSET_ID) | (direction << OFFSET_DIRECTION)
+        byte2 = (speed << OFFSET_MAGNITUDE)
+        packet = bytes([byte1, byte2])
+        #print packet
+        #print self.bytes_to_strings(packet)
 
-    readPackets()
-    print getSpeed(1)
-    setSpeed(1, 0, 0)
-    time.sleep(1)
+        packet = '\x00\x80'
+        print "sending: '{}'".format([ord(p) for p in packet])
+        self.port.write(packet)
+
+    def main(self):
+        while True:
+            '''
+            self.readPackets()
+            #print getSpeed(0)
+            self.setSpeed(0, 100, 0)
+            time.sleep(1)
+            '''
+            #print getSpeed(0)
+            self.setSpeed(0, 0, 0)
+            self.readPackets()
+            time.sleep(1)
+
+if __name__ == '__main__':
+    serialManager = SerialManager()
+    serialManager.main()
