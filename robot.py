e3dbef6437a1a8af6fd07b2feb8d1c32fa14d7ee
Sean Rettig <seanrettig@gmail.com>
2015-04-30 15:35:42 -0700

    use 3 byte packets to communicate with microcontroller

--- robot.py
+++ robot.py
@@ -8,7 +8,8 @@ import time
 OFFSET_TYPE = 6
 OFFSET_ID = 1
 OFFSET_DIRECTION = 0
-OFFSET_MAGNITUDE = 0
+OFFSET_MAGNITUDE1 = 8
+OFFSET_MAGNITUDE2 = 0
 PACKET_DEVICE = 0b00
 PACKET_RELOAD = 0b01
 PACKET_SWAT = 0b10
@@ -31,12 +32,12 @@ class SerialManager(object):
             time.sleep(1)
 
     def readPackets(self):
-        while self.port.inWaiting() >= 2:
-            packet = self.port.read(2)
+        while self.port.inWaiting() >= 3:
+            packet = self.port.read(3)
             #print "received: '{}'".format([ord(p) for p in packet])
-            byte1, byte2 = map(ord, packet)
+            byte1, byte2, byte3 = map(ord, packet)
             deviceid = byte1 << (8 - OFFSET_TYPE) >> (8 - OFFSET_TYPE + OFFSET_ID) << OFFSET_ID
-            magnitude = byte2
+            magnitude = (byte2 << OFFSET_MAGNITUDE1) | (byte3 << OFFSET_MAGNITUDE2)
             self.speeds[deviceid] = magnitude
             #print "device {} ({}) updated with magnitude {} ({})".format(deviceid, dbyte, magnitude, mbyte)
 
@@ -45,8 +46,9 @@ class SerialManager(object):
 
     def setSpeed(self, deviceid, speed=0, direction=0):
         byte1 = (PACKET_DEVICE << OFFSET_TYPE) | (deviceid << OFFSET_ID) | (direction << OFFSET_DIRECTION)
-        byte2 = (speed << OFFSET_MAGNITUDE)
-        packet = ''.join(chr(b) for b in [byte1, byte2])
+        byte2 = (speed >> OFFSET_MAGNITUDE1) & 0b11111111
+        byte3 = (speed >> OFFSET_MAGNITUDE2) & 0b11111111
+        packet = ''.join(chr(b) for b in [byte1, byte2, byte3])
 
         print "sending: '{}'".format([ord(p) for p in packet])
         self.port.write(packet)
