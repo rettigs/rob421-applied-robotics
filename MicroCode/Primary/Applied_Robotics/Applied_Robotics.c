1e7d3901420e9e60a0464edd840f327a4256960c
ambrosmj <ambrosm@onid.oregonstate.edu>
2015-05-27 21:32:20 -0700

    Updates
    Added bunch of stuff, fixed some stuff, needs to be tested.

--- MicroCode/Primary/Applied_Robotics/Applied_Robotics.c
+++ MicroCode/Primary/Applied_Robotics/Applied_Robotics.c
@@ -29,9 +29,11 @@ unsigned long lastRotation = 0;
 unsigned int clockRate;
 volatile bool rotationUpdated = 0;
 volatile float RPM; 
+volatile uint8_t timeoutCounter = 0;
+volatile bool timeoutCheck = 0; 
 
 volatile uint8_t uartData[3] = {0,0,0};
-volatile char i = 0;
+volatile uint8_t i = 0;
 volatile bool uartPacketReady = false; 
 
 /*PID Global Variables*/
@@ -63,12 +65,6 @@ void uartInit(void){
 	//enable usart interrupts
 	UCSR0B |= (1<<RXCIE0);
 	//set baud to 9600
-//	clockRate = (F_CPU/(16*BAUD))-1;
-//	UBRR0H = (unsigned char)(clockRate>>8);
-//	UBRR0L = (unsigned char)clockRate;
-	//set to 115200 Baud rate
-//	UBRR0H = 0x00;
-//	UBRR0L = 0x08;
 	UBRR0H = 0x00;
 	UBRR0L = 0x67;
 }
@@ -76,8 +72,8 @@ void uartInit(void){
 
 void timer0Init(){
 	//Timekeeper timer	
+	
 	TIMSK0 |= (1<<TOIE0);
-
 	//Clk divider by 256
 	TCCR0B |= (1<<CS02);
 	
@@ -92,7 +88,7 @@ void timer1Init(){
 	//Fast PWM Top set by OCR1A (Non-Inverting PWM)
 	TCCR1A |= (1<<COM1A1) | (1<<COM1B1) | (1<<WGM11) | (1<<WGM10);
 	//Clock divider 256. (overflow every 1.048 sec)
-	TCCR1B |= (1<<WGM12) | (1<<WGM12) | (1<<CS10);
+	TCCR1B |= (1<<WGM12) | (1<<WGM13) | (1<<CS10);
 	DDRB |= (1<<PB5) | (1<<PB6);
 	//set register 
 	OCR1A = 0;
@@ -107,7 +103,6 @@ void timer2Init(){
 	//Clear OCA2 on Compare Match. Set OC2A at bottom. 
 	TCCR2A |= (1<<COM2A1) | (1<<COM2B1) | (1<<WGM20) | (1<<WGM21);
 	//Put in PWM waveform Gen mode. Clock divider set to CLK/256
-//	TCCR2B |= (1<<WGM22) | (1<<CS22) | (1<<CS21);
 	TCCR2B |= (1<<CS20); 
 	//TOP is set by value in OCR2A register
 	//Should be able to set OCR2A to PIDoutput 
@@ -190,6 +185,18 @@ void uartSends(char s[]){
 	}
 }
 
+void arbitrationInit(){
+	//Set PG2 as Input enable pullup
+	//PG2 is DIGITAL 39
+	DDRG &= ~(1<<PG2);
+	PORTG |= (1<<PG2);
+	//Can connect this second wire to any ground pin. 
+	//Set PG0 as grounded output 
+	//PG0 is DIGITAL 41
+	DDRG |= (1<<PG0); 
+	PORTG &= ~(1<<PG0); 
+}
+
 void rampMotorSpeed(uint8_t newSpeed){
 	static int motorSpeed = 0;
 	//Ramp up the motor
@@ -230,11 +237,10 @@ void rampMotorSpeed(uint8_t newSpeed){
 void driveStepper(uint16_t steps, bool direction){
 	//Reset PC0-PC3 to zero
 	PORTC &= ~((1<<PC0) | (1<<PC1) | (1<<PC2) | (1<<PC3));
-//	PORTC |= (1<<PC0) | (1<<PC1);
 	//PC0-PC3 are used for stepper control
 	//DFRobot MC
 	//PC0=37=E2,   PC1=36=E1,   PC2=35=M2,   PC3=34=M1 
-	//Red Motor Contorller
+	//Red Motor Controller
 	//PC0=37=In1		PC2=35=In2
 	//PC1=36=In3		PC3=34=In4
 	//YELLOW and RED go to POSITIVE Terminal
@@ -364,33 +370,21 @@ void PIDsetControllerDirection(int Direction)
 	controllerDirection = Direction;
 }
 
-void PIDenable(void){
-	//Turn on PID and start setting it
-	//Allow PID to automatically control settings
-	PIDsetMode(AUTOMATIC);
-	//Positive proportional control
-	PIDsetControllerDirection(DIRECT);
-	//set sample time in ms - should be handled in interrupt?
-	PIDsetSampleTime(50);
-	//Minimum and Maximum output values
-	PIDsetOutputLimits(0,255);
-	//Set PID params (Kp, Ki, Kd)
-	PIDsetTunings(6,0.6,0.6);
-	//Start controller
-	PIDinitialize();
-	//Set Setpoint to 0 RPM
-	Setpoint = 0;
-}
-
 int main(void)
 {
 	uartInit();
+	//Timekeeper
 	timer0Init();
+	//16bit PWM
 	timer1Init();
+	//8bit PWM - unused
 //	timer2Init();
+	//50Hz Servo timer
 	timer3Init();
+	//Encoder counter
 	timer5Init(); 
 	externalInterrupts();
+	arbitrationInit();
 	
 	PIDsetTunings(4,2,0);
 	PIDsetSampleTime(4);
@@ -406,14 +400,13 @@ int main(void)
 	//PC0-PC3 are used for stepper control
 	//PC0=37,   PC1=36,   PC2=35,   PC3=34
 	DDRC |= (1<<PC0) | (1<<PC1) | (1<<PC2) | (1<<PC3);
-		
+	
 	sei();
 	
     while(1)
 	    {
 				
-				if(uartPacketReady == true){
-					//For 19V source 000f50 is about 12V at the motor. 
+				if(uartPacketReady == true){ 
 					//Echo back received data
 					uartSendc(uartData[0]);
 					uartSendc(uartData[1]);
@@ -431,6 +424,14 @@ int main(void)
 						OCR3B = 1235;
 					}
 					
+					//Toggle on/off Reloader
+					if(uartData[0] == 0x99){
+						//toggle on/off PWM output
+						TCCR3B ^= ((1<<COM3A1) | (1<<COM3A0));
+						//toggle on/off external interrupts
+						EIMSK ^= ((1<<INT5) | (1<<INT2));
+					}
+					
 					//Motor 0 (launcher) forward control
 					//HEX CODE: 00 XX XX
 					if(uartData[0] == 0b00000000){
@@ -439,8 +440,6 @@ int main(void)
 						//Min distance is 0x51
 						PORTB &= ~(1<<PB7);
 						Setpoint = (uartData[1]<<8) | uartData[2];
-	//					uartSendc(uartData[1]);
-	//					uartSendc(uartData[2]);
 					}	
 					//Motor 0 (launcher) backward control
 					//HEX CODE: 01	XX	XX	
@@ -454,8 +453,9 @@ int main(void)
 					if(uartData[0] == 0b01000000){
 						//Move servo backward.
 						OCR3A = 1235;
-						//Set up external interrupt to stop servo when sensor is touched
-						//Reverse direction of Servo and move backward.
+						//start timeout
+						timeoutCheck = 1;
+						timeoutCounter = 0;
 					}
 					//Carriage (Motor 1) forward control
 					//HEX CODE: 02 XX XX
@@ -473,12 +473,35 @@ int main(void)
 					}
 					uartPacketReady = false;
 				}
+				if(OCR1A >= 1000){
+					if(rotation == 0){
+						OCR1A = 0; 
+						uartSends("MOTOR STALL");
+					}
+				}
 				_delay_ms(1);
 		}
 }
 
 
 ISR(TIMER0_OVF_vect){
+	//increment counter every ~50ms
+	if((tick == 30) | (tick == 60)){
+		//If servo is moving for more than 1.5s it is probably stalled.
+		if(timeoutCounter >= 3){
+			//Stop servo
+			OCR3A = 1171;
+		}
+		//Check if timeout flag is set (the servo is moving)
+		if(timeoutCheck == 1){
+			timeoutCounter++; 
+		}
+		//PG2 is connected as input with pullup. Other wire is grounded.   
+		//IF switch is thrown then PG2 is pulled low signaling our turn has started. 
+		if(PING & (1<<PING2)){
+			uartSendc(0x77); 
+		}
+	}
 	if(tick == 60){
 			uartSendc(rotation);
 			tick = 0;
@@ -490,25 +513,29 @@ ISR(TIMER0_OVF_vect){
 		TCNT5 = 0; 
 		
 		PIDinput = rotation;		
-		PIDcompute();
-		//Send back data
-//		uartSendc((uint16_t)RPM>>8);
-//		uartSendc((uint8_t)RPM);
-	
+		PIDcompute();	
 }
 
 ISR(INT5_vect){
 	//Forward limit switch - stop the motor from moving.
 	//Switch should be attached to ground and Digital 3
 	OCR3A = 1171; 
-//	uartSendc(0xff);
+	//Motor is now stopped. Reset timeouts
+	timeoutCheck = 0;
+	timeoutCounter = 0; 
 }
 ISR(INT2_vect){
 	//Rear limit switch - reverses direction of motor.
 	//Should be attached to ground and Digital 19.
+	//Turn off timeoutCheck and reset timeoutCount
+	timeoutCheck = 0;
+	timeoutCounter = 0;
 	OCR3A = 1171;
 	_delay_ms(20);
+	//Start timeoutCheck when motor changed direction
+	timeoutCheck = 1;
 	OCR3A = 1157;
+	
 }
 
 ISR(USART0_RX_vect){
