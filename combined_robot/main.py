d2bde21a123a3576d31dc3d0cd8083f03368bc55
Sean Rettig <seanrettig@gmail.com>
2015-06-04 15:17:40 -0700

    Revert "Revert "try to integrate bounce shot detection into combined robot""
    This reverts commit 0f5bed5f695981c497299d0544b9ffeb7abd47fc.

--- combined_robot/main.py
+++ combined_robot/main.py
@@ -11,7 +11,8 @@ def usage():
     print 'Usage: {0} [-s serialdevice] [-c capturedevice] [-h] [-v]... [-d]...'.format(sys.argv[0])
     print '\t-h\tview this help'
     print '\t-s\tspecify a serial device to use, defaults to "/dev/ttyACM0"'
-    print '\t-c\tspecify a video capture device to use, defaults to "/dev/video0"'
+    print '\t-m\tspecify the main video capture device to use, defaults to "/dev/video0"'
+    print '\t-b\tspecify the bounce shot video capture device to use, defaults to "/dev/video1"'
     print '\t-v\tenable more verbose messages; use -vv for more even more messages'
     print '\t-d\tenable debug messages; use -dd for more even more messages'
     sys.exit(2)
@@ -20,11 +21,12 @@ if __name__ == '__main__':
 
     # Defaults
     serialDevice = '/dev/ttyACM0'
-    captureDevice = '0'
+    mainCaptureDevice = '0'
+    bounceCaptureDevice = '0'
 
     # Parse arguments
     try:
-        opts, args = getopt.getopt(sys.argv[1:], "s:c:dvh")
+        opts, args = getopt.getopt(sys.argv[1:], "s:m:b:dvh")
     except getopt.GetoptError as err:
         # print help information and exit:
         print str(err) # will print something like "option -a not recognized"
@@ -32,14 +34,16 @@ if __name__ == '__main__':
     for o, a in opts:
         if o == "-d":
             global debug
-            debug += 1
+            #debug += 1
         elif o == "-v":
             global verbose
-            verbose += 1
+            #verbose += 1
         elif o == "-s":
             serialDevice = a
-        elif o == "-c":
-            captureDevice = a
+        elif o == "-m":
+            mainCaptureDevice = a
+        elif o == "-b":
+            bounceCaptureDevice = a
         else:
             usage()
     
@@ -54,4 +58,4 @@ if __name__ == '__main__':
     #opts = dict(opts)
 
     #App(videoDevice, paused = '--pause' in opts).run()
-    App(captureDevice, robotq, appq, launchspeed).run()
+    App(mainCaptureDevice, bounceCaptureDevice, robotq, appq, launchspeed).run()
